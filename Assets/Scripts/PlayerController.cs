b72a39a15c6a81c46de071089b52cae0bc71320a
kevinluuhuynh <kevin.luuhuynh@gmail.com>
2020-06-11 17:19:51 -0400

    player jump function and move left/right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public float speed;
+   public float jumpForce;
+   private float moveInput;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private Rigidbody2D rb;
+
+   private bool facingRight = true;
+
+   private bool isGrounded;
+   public Transform groundCheck;
+   public float checkRadius;
+   public LayerMask whatIsGround;
+
+
+   private int extraJumps;
+   public int extraJumpsValue;
+
+
+   void Start(){
+       extraJumps = extraJumpsValue;
+       rb = GetComponent<Rigidbody2D>();
+   }
+
+   void FixedUpdate(){
+
+       isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+       moveInput = Input.GetAxisRaw("Horizontal");
+       rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+       if(facingRight == false && moveInput > 0) {
+           Flip();
+       } else if(facingRight == true && moveInput < 0) {
+           Flip();
+       }
+   }
+
+   void Update(){
+
+       if(isGrounded == true) {
+           extraJumps = extraJumpsValue;
+       }
+       if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0){
+           rb.velocity = Vector2.up * jumpForce;
+           extraJumps--;
+       }else if(Input.GetKeyDown(KeyCode.Space)  && extraJumps == 0 && isGrounded == true) {
+           rb.velocity = Vector2.up * jumpForce;
+       }
+   }
+
+   void Flip(){
+
+       facingRight = !facingRight;
+       Vector3 Scaler = transform.localScale;
+       Scaler.x *= -1;
+       transform.localScale = Scaler;
+   }
 }
