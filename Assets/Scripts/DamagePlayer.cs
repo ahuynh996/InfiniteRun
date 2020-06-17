e344283b0eb025b098f45838906e2c546e1bd25d
kevinluuhuynh <kevin.luuhuynh@gmail.com>
2020-06-17 19:32:58 -0400

    Added player health damage

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public int playerHealth = 30;
+    int damage = 10;
+
+    private void Start()
+    {
+        print(playerHealth);
+        
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Image healthBar;
+    public float maxHealth = 3f;
+    public float currHealth = 0f;
+    public bool alive = true;
+
+
+    void Start()
+    {
+        alive = true;
+        currHealth = maxHealth;
+        //InvokeRepeating("DoDamage", 1f, 5f);
+    }
+
+    //Do damage over time
+    /*
+    void DoDamage()
+    {
+        TakeDamage(10f);
+    }
+    */
+
+    public void TakeDamage(float dmgAmount)
+    {
+        if(!alive)
+        {
+            return;
+        }
+
+        if(currHealth <= 0)
+        {
+            currHealth = 0;
+            alive = false;
+            //gameObject.SetActive(false);
+        }
+
+        currHealth -= dmgAmount;
+        SetHealthBar();
+        Debug.Log("Current Health " + currHealth);
+    }
+    // Update is called once per frame
+    void SetHealthBar()
+    {
+        float myHealth = currHealth / maxHealth;
+        //healthBar.transform.localScale = new Vector3(Mathf.Clamp(myHealth, 0f, 1f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+}
