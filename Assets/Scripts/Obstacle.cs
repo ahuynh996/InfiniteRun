4762e9047e9b1dedf37ad70ff9de45cdd7395e67
kevinluuhuynh <kevin.luuhuynh@gmail.com>
2020-06-20 13:13:41 -0400

    Blocks spawn randomly and move to the left

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    float speed = 200f;
+   // float speed = 200f;
     float damage = 1f;
 
     // Start is called before the first frame update
@@ -16,7 +16,7 @@ public class Obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (Vector3.up * Time.deltaTime * speed);
+       // transform.Rotate (Vector3.up * Time.deltaTime * speed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject blockObstacle;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(blockWave());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < screenBounds.x * 2)
+        {
+            
+        }
+    }
+
+    private void spawnBlocks()
+    {
+        GameObject a = Instantiate(blockObstacle) as GameObject;
+        a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator blockWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnBlocks();
+        }
+        
+    }
+}
