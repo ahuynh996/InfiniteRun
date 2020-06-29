559b5a7615718a769db715cf293674fe71d71a4d
ewdlop <ray810815@gmail.com>
2020-06-29 16:43:58 -0400

    Hook up basic Mutiliplayer functionality

---
+++ Assets/Scripts/Raymond/PlayerControllerMirror.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerControllerMirror : NetworkBehaviour
+{
+    [SerializeField]
+    private bool facingRight = true;
+    [SerializeField]
+    private bool isGrounded;
+    [SerializeField]
+    private bool alive = true;
+    [SerializeField]
+    private float checkRadius;
+    [SerializeField]
+    private int maxHealth = 10;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float moveInput;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Color defaultColor;
+    [SerializeField]
+    private Color hurtColor;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private Renderer rend;
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+    [SerializeField]
+    private Transform groundCheck;
+
+    [Header("Game Stats")]
+    [SerializeField]
+    [SyncVar(hook = nameof(OnHealthChange))]
+    private int currentHealth = 10;
+    [SerializeField]
+    [SyncVar]
+    private bool isTakingDmg;
+
+    //void FixedUpdate()
+    //{
+    //    //if (!isLocalPlayer) {
+    //    //    return;
+    //    //}
+
+    //    //isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+    //    //moveInput = Input.GetAxisRaw("Horizontal");
+    //    //rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
+
+    //    //if (facingRight == false && moveInput > 0) {
+    //    //    Flip();
+    //    //} else if (facingRight == true && moveInput < 0) {
+    //    //    Flip();
+    //    //}
+    //}
+
+    void Update() {
+
+        if(!isLocalPlayer) {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    private void OnHealthChange(int oldHealth, int newHealth) {
+        StartCoroutine(ClientBlink());
+        //SetHealthBar(newHealth);
+    }
+
+    [ServerCallback]
+    public void TakeDamage(int dmgAmount)
+    {
+        if(isTakingDmg) {
+            return;
+        }
+        isTakingDmg = true;
+        currentHealth -= dmgAmount;
+        //SetHealthBar(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            alive = false;
+            //FindObjectOfType<GameManager>().EndGame();
+        }
+        StartCoroutine(ServerBlink());
+    }
+    IEnumerator ServerBlink() {
+        yield return new WaitForSeconds(4f);
+        isTakingDmg = false;
+    }
+
+    IEnumerator ClientBlink() {
+        for (int i = 0; i < 20; ++i) {
+            rend.material.color = Color.black;
+            yield return new WaitForSeconds(0.1f);
+            rend.material.color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
--- Assets/Scripts/Raymond/PlayerTemplate.cs
+++ Assets/Scripts/Raymond/PlayerTemplate.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using Mirror;
 
-//NetworkBehaivour requires NetWorkIdentity(For remote call privilege and state synchronization)
-public class Player : NetworkBehaviour
+//NetworkBehaivour requires NetWorkIdentity Component(for remote call privilege and state synchronization)
+public class PlayerTemplate : NetworkBehaviour
 {
     [SerializeField]
     private float speed;
@@ -11,8 +11,8 @@ public class Player : NetworkBehaviour
     [SerializeField]
     private GameObject prefab;
 
-    /**Addition to auto syncing, this all of the other's player's character's to synchornzie their health 
-    to the corresponding copy of new player right after joining I think**/
+    /**Addition to auto syncing, this allow all of the other's player's character's to synchornzie their health 
+    to the corresponding copy of a newly joined player I think**/
     [SyncVar(hook=nameof(OnHealthChange))]
     private int currentHealth;
 
@@ -23,7 +23,7 @@ public class Player : NetworkBehaviour
         }
         //Dont wanna do something to your local copy of other player's character by accident lol
         if (isLocalPlayer) {
-            //NetworkTransform Component will synch the transform I think
+            //NetworkTransform Component will synch the gameobject's transform among all the clients
             //this happen immediately in the client!
             float movement = Input.GetAxis("Horizontal");
             GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0.0f);
@@ -40,27 +40,27 @@ public class Player : NetworkBehaviour
         }
 
 
-        //this might be delayed due to LAG
-        //this can create a situation where a player's character is dead in the server's point of view
-        //, but not dead in the client's point of view lol
+        //Health reduction might be delayed due to LAG in the client side
+        //this can create a situation where a player's character is dead from the server's point of view
+        //, but is not dead from the client's point of view lol
 
-        //there is a secret ClientRpc call here I think, to sync the health. Syncvar makes our lives easier
-        //invoke OnHeathChange hook
+        //I think there is a secret ClientRpc call here, to sync the health. Syncvar makes our lives easier
+        //and invoke OnHeathChange hook
         currentHealth -= amount;
-        //Play hurt animation on the server's local client's instance here
-        
+
         //let the server decides whether if a player's character'death is legitmate
         if (currentHealth <= 0) {
-            //let the server decides whether if a player's character shoud be respawned(not from the hook!)
+            //let the server decides whether if a player's character is allow to be respawned(not from the hook)
             RpcRespawn();
         }
     }
-    /**This method is called when the SyncVar currentHealth(remote clients' local copy) is changed by the server**/
+
+    /**Hook. This method is called when the SyncVar currentHealth(remote clients' local copy) is changed by the server**/
     private void OnHealthChange(int oldHealth, int newHealth) {
         
         if (newHealth <= 0) {
             //Play death animation
-            //dont respawn here
+            //Dont respawn here
         }
         else {
             //Play hurt animation
@@ -68,14 +68,14 @@ public class Player : NetworkBehaviour
     }
 
     /**This method is invoke on the server's local client's instance of the player's instance
-    The server replicates the bullet among all clients.
+    Then, the server replicates the bullet among all clients.
     If the connection is lost, no bullet will show up*/
     [Command]
     void CmdShoot()
     {
         //run on the server!
-        //validate the player's character's heath to make sure the player's request is still legitmate
-        //the player might doesnt know the character is dead due to lag
+        //validate the player's character's heath to make sure the player's request is legitmate
+        //the player might doesnt know the character is dead due to a delay or LAG
         if(currentHealth > 0) 
         {
             GameObject bullet = Instantiate(this.prefab, transform.position, Quaternion.identity);
