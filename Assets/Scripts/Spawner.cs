fa8ad5b18c7262240a21469483685ffaceb3ac8d
Alan Huynh <ahuynh996@gmail.com>
2020-07-06 20:50:35 -0400

    update border with art but still needs fix on position

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject blockObstacle;
+    public GameObject topBorder;
+    public GameObject bottomBorder;
     public float respawnTime = 1.0f;
+    public float borderRespawnTime;
     private Vector2 screenBounds;
 
     // Start is called before the first frame update
@@ -13,6 +17,8 @@ public class Spawner : MonoBehaviour
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         StartCoroutine(blockWave());
+        StartCoroutine(borderWave());
+
     }
 
     // Update is called once per frame
@@ -27,7 +33,17 @@ public class Spawner : MonoBehaviour
     private void spawnBlocks()
     {
         GameObject a = Instantiate(blockObstacle) as GameObject;
+
         a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    private void spawnBorder()
+    {
+        GameObject top = Instantiate(topBorder) as GameObject;
+        GameObject bottom = Instantiate(bottomBorder) as GameObject;
+
+        top.transform.position = new Vector2(screenBounds.x * -2, screenBounds.y);
+        bottom.transform.position = new Vector2(screenBounds.x * -2, -screenBounds.y);
     }
 
     IEnumerator blockWave()
@@ -39,4 +55,16 @@ public class Spawner : MonoBehaviour
         }
         
     }
+    
+    IEnumerator borderWave()
+    {
+       while(true)
+        {
+            yield return new WaitForSeconds(borderRespawnTime);
+            spawnBorder();
+
+        }
+    }
+    
+
 }
