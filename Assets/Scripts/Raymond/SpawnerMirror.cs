be4cf5526bbdf2f0ea48e51820707ef491aaa8de
ewdlop <ray810815@gmail.com>
2020-07-07 20:27:59 -0400

    sync up with Alan's border change

--- Assets/Scripts/Raymond/SpawnerMirror.cs
+++ Assets/Scripts/Raymond/SpawnerMirror.cs
@@ -8,6 +8,10 @@ public class SpawnerMirror : NetworkBehaviour
     [SerializeField]
     private GameObject blockObstacle;
     [SerializeField]
+    private GameObject topBorder;
+    [SerializeField]
+    private GameObject bottomBorder;
+    [SerializeField]
     private float respawnTime = 1.0f;
     [SerializeField]
     private Vector2 screenBounds;
@@ -16,19 +20,39 @@ public class SpawnerMirror : NetworkBehaviour
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         StartCoroutine(BlockWave());
+        StartCoroutine(BorderWave());
     }
 
-    private void SpawnBlocks() {
+    private void SpawnBlocks()
+    {
         GameObject obstacle = Instantiate(blockObstacle);
         obstacle.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
         NetworkServer.Spawn(obstacle);
     }
 
-    IEnumerator BlockWave() {
+    private void SpawnBorder()
+    {
+        GameObject top = Instantiate(topBorder);
+        GameObject bottom = Instantiate(bottomBorder);
+        top.transform.position = new Vector2(screenBounds.x * -2, screenBounds.y);
+        bottom.transform.position = new Vector2(screenBounds.x * -2, -screenBounds.y);
+        NetworkServer.Spawn(top);
+        NetworkServer.Spawn(bottom);
+    }
+
+    IEnumerator BlockWave()
+    {
         while (true) {
             yield return new WaitForSeconds(respawnTime);
             SpawnBlocks();
         }
     }
+    IEnumerator BorderWave()
+    {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnBorder();
+        }
 
+    }
 }
