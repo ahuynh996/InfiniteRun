b7489c13253cf50852b1ed7d2d5d34cfb8a565fb
Alan Huynh <ahuynh996@gmail.com>
2020-07-10 00:26:11 -0400

    cleaned up code and extended obstacles length

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,49 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject blockObstacle;
-    public GameObject topBorder;
-    public GameObject bottomBorder;
     public float respawnTime = 1.0f;
-    public float borderRespawnTime;
     private Vector2 screenBounds;
+    private float minY = -5.0f;
+    private float maxY = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         StartCoroutine(blockWave());
-        StartCoroutine(borderWave());
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < screenBounds.x * 2)
-        {
-            
-        }
+     
     }
 
     private void spawnBlocks()
     {
-        GameObject a = Instantiate(blockObstacle) as GameObject;
+        GameObject blockSpawnPosition = Instantiate(blockObstacle) as GameObject;
 
-        a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
-    }
-
-    private void spawnBorder()
-    {
-        GameObject top = Instantiate(topBorder) as GameObject;
-        GameObject bottom = Instantiate(bottomBorder) as GameObject;
-
-        top.transform.position = new Vector2(screenBounds.x * -2, screenBounds.y);
-        bottom.transform.position = new Vector2(screenBounds.x * -2, -screenBounds.y);
+        blockSpawnPosition.transform.position = new Vector3(transform.position.x, Random.Range(minY, maxY), transform.position.z);
     }
 
     IEnumerator blockWave()
@@ -55,16 +41,4 @@ public class Spawner : MonoBehaviour
         }
         
     }
-    
-    IEnumerator borderWave()
-    {
-       while(true)
-        {
-            yield return new WaitForSeconds(borderRespawnTime);
-            spawnBorder();
-
-        }
-    }
-    
-
 }
