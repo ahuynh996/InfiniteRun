3855130c0fb8c400e56bc6cd2483904a68f29e82
alvin609 <68091015+alvin609@users.noreply.github.com>
2020-08-11 22:05:17 -0400

    Score Counter
    Created the increment score counter after player passes obstacle.

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    static int score = 0;
+
+    public Text scoreText;
+
+    void Update()
+    {
+
+        scoreText.text = score.ToString();
+    }
+    public static void IncrementScore()
+    {
+        score++;
+    }
+}
